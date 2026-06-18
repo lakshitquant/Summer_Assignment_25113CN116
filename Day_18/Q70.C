78abe4fb55b1c758f04de9b4eec02429283f2f71
lakshitquant <lakshit0003@gmail.com>
2026-06-18 09:57:41 +0530

    addday_18

---
+++ Day_18/Q70.C
@@ -0,0 +1,40 @@
+/*
+Program: Selection Sort
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[50], n, i, j, minIndex, temp;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = 0; i < n - 1; i++)
+    {
+        minIndex = i;
+
+        for(j = i + 1; j < n; j++)
+        {
+            if(arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    printf("Sorted Array: ");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
---
+++ Day_18/Q71.C
@@ -0,0 +1,50 @@
+
+//Program: Binary Search
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[50], n, i;
+    int key, low, high, mid;
+    int found = 0;
+
+    printf("Enter size of sorted array: ");
+    scanf("%d", &n);
+
+    printf("Enter sorted elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key)
+        {
+            found = 1;
+            break;
+        }
+        else if(key > arr[mid])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    if(found)
+        printf("Element found at position %d", mid + 1);
+    else
+        printf("Element not found");
+
+    return 0;
+}
